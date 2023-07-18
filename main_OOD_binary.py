30ef0494b48cb9e341769e5e2b05baaeae75c646
xysong <xysong25@gmail.com>
2023-07-17 22:25:43 -0400

    Add MC

--- main_OOD_binary.py
+++ main_OOD_binary.py
@@ -94,135 +94,157 @@ os.makedirs(file_root, exist_ok=True)
 file_name = file_root + 'log.txt'
 
 best_test_acc = 0
-best_OOD_dist = 1
+best_tpr95 = 0
+best_tpr99 = 0
 best_then01_dist = 1
 
 with open(file_name, 'a') as f:
     f.write('DenseNet 100 f ' + InD_Dataset + ' InD ' + OOD_Dataset + ' OOD experiment epoch = ' + str(num_epochs) + ' beta = ' + str(beta[0]) + ' OOD Size = ' + str(OOD_batch_size) + '\n')
 
+tpr95_lst = []
+tpr99_lst = []
+mc = 3
 
-for epoch in tqdm(range(num_epochs)):
-    count = 0
-    for (InD_images, InD_labels), (OOD_images, OOD_labels) in zip(InD_train_loader, cycle(OOD_train_loader)):
-    #for InD_images, InD_labels in InD_train_loader:
-        model.train()
-        
-        ##load a batch of ood data
-        ##change the label of ood data
-        OOD_labels[:] = num_class[0]
-
-        images = torch.cat([InD_images, OOD_images], dim=0)
-        labels = torch.cat([InD_labels, OOD_labels], dim=0)
-
-        ##shuffle the order of InD and OOD samples
-        idx = torch.randperm(images.shape[0])
-        images = images[idx].view(images.size())
-        labels = labels[idx].view(labels.size())
-
-        # Transfering images and labels to GPU if available
-        images, labels = images.to(device), labels.to(device)
-
-        train = Variable(images)
-        #print(train.shape)
-        labels = Variable(labels)
-
-        # Forward pass 
-        outputs = model(train)
-        
-        loss = NLLWOOD_l(outputs, labels, num_class, beta, device)
-
-        # Initializing a gradient as 0 so there is no mixing of gradient among the batches
-        optimizer.zero_grad()
-
-        # Propagating the error backward
-        loss.backward()
-
-        # Optimizing the parameters
-        optimizer.step()
-
-        count += 1
-        
-        
-        # Testing the model
-        if not (count % 800):    # It's same as "if count % 100 == 0"
-            total = 0
-            correct = 0
-            InD_test_sink_dist_list = []
-            model.eval()
-            ##InD samples test
-            for images, labels in InD_test_loader:
-                images, labels = images.to(device), labels.to(device)
-                labels_list.append(labels)
-
-                test = Variable(images.view(images.size()))
-
-                outputs = model(test)
-
-
-                InD_sink_dist = sink_dist_test_v2(outputs, labels, num_class[0], device).cpu().detach().numpy()
-                InD_test_sink_dist_list.append(InD_sink_dist)
-
-
-                predictions = torch.max(outputs, 1)[1].to(device)
-                predictions_list.append(predictions)
-
-                correct += (predictions == labels).sum()
-
-                total += len(labels)
+for mc_num in range(mc):
+    for epoch in tqdm(range(num_epochs)):
+        count = 0
+        for (InD_images, InD_labels), (OOD_images, OOD_labels) in zip(InD_train_loader, cycle(OOD_train_loader)):
+        #for InD_images, InD_labels in InD_train_loader:
+            model.train()
             
-            InD_test_mean_sink_dist = np.concatenate(InD_test_sink_dist_list, axis=0)
-            InD_sink_mean = np.mean(InD_test_mean_sink_dist)
+            ##load a batch of ood data
+            ##change the label of ood data
+            OOD_labels[:] = num_class[0]
+
+            images = torch.cat([InD_images, OOD_images], dim=0)
+            labels = torch.cat([InD_labels, OOD_labels], dim=0)
+
+            ##shuffle the order of InD and OOD samples
+            idx = torch.randperm(images.shape[0])
+            images = images[idx].view(images.size())
+            labels = labels[idx].view(labels.size())
+
+            # Transfering images and labels to GPU if available
+            images, labels = images.to(device), labels.to(device)
+
+            train = Variable(images)
+            #print(train.shape)
+            labels = Variable(labels)
+
+            # Forward pass 
+            outputs = model(train)
             
-            accuracy = correct * 100 / float(total)
+            loss = NLLWOOD_l(outputs, labels, num_class, beta, device)
 
-            if accuracy >= best_test_acc:
-                best_test_acc = accuracy
-                torch.save(model.state_dict(), '%s/acc_model.t7' % file_root)
+            # Initializing a gradient as 0 so there is no mixing of gradient among the batches
+            optimizer.zero_grad()
 
-            ##OOD samples test
-            OOD_test_sink_dist_list = []
-            total = 0
-            for images, labels in OOD_test_loader:
-                labels[:] = 0
-                images, labels = images.to(device), labels.to(device)
-                labels_list.append(labels)
+            # Propagating the error backward
+            loss.backward()
 
-                test = Variable(images.view(images.size()))
+            # Optimizing the parameters
+            optimizer.step()
 
-                outputs = model(test)
-                total += len(labels)
-                OOD_sink_dist = sink_dist_test_v2(outputs, labels, num_class[0], device).cpu().detach().numpy()
-                OOD_test_sink_dist_list.append(OOD_sink_dist)
+            count += 1
             
-            OOD_test_mean_sink_dist = np.concatenate(OOD_test_sink_dist_list, axis=0)
-            OOD_sink_mean = np.mean(OOD_test_mean_sink_dist)
-
-            # 0.95 TNR
-            thresh95 = np.quantile(InD_test_mean_sink_dist, 0.95)
-            tpr95 = 1 - OOD_test_mean_sink_dist[OOD_test_mean_sink_dist<=thresh95].shape[0] / float(OOD_test_mean_sink_dist.shape[0])
-            thresh95 = float(format(thresh95, '.4g'))
-            tpr95 = float(format(tpr95, '.4g'))
-
-            # 0.99 TNR
-            thresh99 = np.quantile(InD_test_mean_sink_dist, 0.99)
-            tpr99 = 1 - OOD_test_mean_sink_dist[OOD_test_mean_sink_dist<=thresh99].shape[0] / float(OOD_test_mean_sink_dist.shape[0])
-            thresh99 = float(format(thresh99, '.4g'))
-            tpr99 = float(format(tpr99, '.4g'))
-
-            accuracy = float(format(accuracy, '.4g'))
             
-            log = "Epoch: {}, Iteration: {}, Loss: {}, Accuracy: {}%, InD_sink: {}, OOD_sink: {}, OOD_95_TPR: {}, OOD_99_TPR: {}".format(epoch, count, loss[0], accuracy, InD_sink_mean, OOD_sink_mean, tpr95, tpr99)
-            print(log)
+            # Testing the model
+            if not (count % 800):    # It's same as "if count % 100 == 0"
+                total = 0
+                correct = 0
+                InD_test_sink_dist_list = []
+                model.eval()
+                ##InD samples test
+                for images, labels in InD_test_loader:
+                    images, labels = images.to(device), labels.to(device)
+                    labels_list.append(labels)
 
-            with open(file_name, 'a') as f:
-                f.write(log+'\n')
-            if tpr95 <= best_OOD_dist:
+                    test = Variable(images.view(images.size()))
+
+                    outputs = model(test)
+
+
+                    InD_sink_dist = sink_dist_test_v2(outputs, labels, num_class[0], device).cpu().detach().numpy()
+                    InD_test_sink_dist_list.append(InD_sink_dist)
+
+
+                    predictions = torch.max(outputs, 1)[1].to(device)
+                    predictions_list.append(predictions)
+
+                    correct += (predictions == labels).sum()
+
+                    total += len(labels)
                 
-                best_OOD_dist = tpr95
-                torch.save(model.state_dict(), '%s/OOD_model_%s_%s.t7' % (file_root, str(accuracy).split('.')[0], 
-                                                                          str(tpr95).split('.')[1]))
-            
-            
+                InD_test_mean_sink_dist = np.concatenate(InD_test_sink_dist_list, axis=0)
+                InD_sink_mean = np.mean(InD_test_mean_sink_dist)
+                
+                accuracy = correct * 100 / float(total)
+
+                if accuracy >= best_test_acc:
+                    best_test_acc = accuracy
+                    torch.save(model.state_dict(), f'%s/acc_{mc_num}_model.t7' % file_root)
+
+                ##OOD samples test
+                OOD_test_sink_dist_list = []
+                total = 0
+                for images, labels in OOD_test_loader:
+                    labels[:] = 0
+                    images, labels = images.to(device), labels.to(device)
+                    labels_list.append(labels)
+
+                    test = Variable(images.view(images.size()))
+
+                    outputs = model(test)
+                    total += len(labels)
+                    OOD_sink_dist = sink_dist_test_v2(outputs, labels, num_class[0], device).cpu().detach().numpy()
+                    OOD_test_sink_dist_list.append(OOD_sink_dist)
+                
+                OOD_test_mean_sink_dist = np.concatenate(OOD_test_sink_dist_list, axis=0)
+                OOD_sink_mean = np.mean(OOD_test_mean_sink_dist)
+
+                # 0.95 TNR
+                thresh95 = np.quantile(InD_test_mean_sink_dist, 0.95)
+                tpr95 = 1 - OOD_test_mean_sink_dist[OOD_test_mean_sink_dist<=thresh95].shape[0] / float(OOD_test_mean_sink_dist.shape[0])
+                thresh95 = float(format(thresh95, '.4g'))
+                tpr95 = float(format(tpr95, '.4g'))
+
+                # 0.99 TNR
+                thresh99 = np.quantile(InD_test_mean_sink_dist, 0.99)
+                tpr99 = 1 - OOD_test_mean_sink_dist[OOD_test_mean_sink_dist<=thresh99].shape[0] / float(OOD_test_mean_sink_dist.shape[0])
+                thresh99 = float(format(thresh99, '.4g'))
+                tpr99 = float(format(tpr99, '.4g'))
+
+                accuracy = float(format(accuracy, '.4g'))
+                
+                log = "Epoch: {}, Iteration: {}, Loss: {}, Accuracy: {}%, InD_sink: {}, OOD_sink: {}, OOD_95_TPR: {}, OOD_99_TPR: {}".format(epoch, count, loss[0], accuracy, InD_sink_mean, OOD_sink_mean, tpr95, tpr99)
+                print(log)
+
+                with open(file_name, 'a') as f:
+                    f.write(log+'\n')
+                
+                if tpr99 >= best_tpr99:
+                    best_tpr99 = tpr99
+
+                if tpr95 >= best_tpr95:
+                    best_tpr95 = tpr95
+                    torch.save(model.state_dict(), f'%s/OOD_model_{mc_num}_%s_%s.t7' % (file_root, str(accuracy).split('.')[0], 
+                                                                            str(tpr95).split('.')[1]))
+    
+    f.write(f"\n MC Iteration {mc_num} ends.\n")
+    tpr95_lst.append(best_tpr95)
+    tpr99_lst.append(best_tpr99)
+
+f.write(f"\n TPR at 95 TNR: {tpr95_lst}")
+f.write(f"\n TPR at 95 TNR MEAN: {np.mean(tpr95_lst)}")
+mad95 = np.mean(np.abs(np.mean(tpr95_lst) - np.array(tpr95_lst)))
+f.write(f"\n TPR at 95 TNR MAD: {mad95}")
+
+f.write(f"\n TPR at 99 TNR: {tpr99_lst}")
+f.write(f"\n TPR at 99 TNR MEAN: {np.mean(tpr99_lst)}")
+mad99 = np.mean(np.abs(np.mean(tpr99_lst) - np.array(tpr99_lst)))
+f.write(f"\n TPR at 99 TNR MAD: {mad99}")
+
+
 
 
 

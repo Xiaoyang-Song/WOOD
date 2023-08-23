239f43df6b569be1d97006726d422f0db944677c
xysong <xysong25@gmail.com>
2023-08-22 23:43:01 -0400

    Update: scheduler

---
+++ jobs/svhn-r2/1024.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account=alkontar1
+#SBATCH --job-name=WDSV1024
+#SBATCH --mail-user=xysong@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-gpu=16GB
+#SBATCH --time=36:00:00
+#SBATCH --output=/home/xysong/WOOD/slurm-jobs/WDSV1024-R2.log
+
+python3 main_OOD_binary.py 0.1 100 60 50 SVHN-07 SVHN-89 3 SVHN 1024 8
